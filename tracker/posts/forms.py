8a1da35c687a9f4ada15dcd38d2ba38bbe8fa2e7
rameshkrishna10 <ramesh.krishna@adroll.com>
2019-05-16 17:57:53 +0530

    regularized the form spacing

--- tracker/posts/forms.py
+++ tracker/posts/forms.py
@@ -11,27 +11,26 @@ from bootstrap_datepicker_plus import DatePickerInput
 from django.forms import ModelForm, widgets, DateTimeField, DateField, DateInput
 
 class CreateArticle(forms.ModelForm):
-
-
-    Year = forms.IntegerField(widget=forms.NumberInput(attrs={'title':'Enter the current Year'}))
+    Year = forms.IntegerField(widget=forms.NumberInput(attrs={'title':'Enter the current Year','style': 'width: 55% !important; resize: vertical !important;'}))
 
     weeknumber = date.today().isocalendar()[1]
-    Week = forms.CharField(initial=weeknumber, widget=forms.TextInput(attrs={'title':'Week number'}))
+    Week = forms.CharField(initial=weeknumber, widget=forms.TextInput(attrs={'title':'Week number','style': 'width: 55% !important; resize: vertical !important;'}))
+
+    Date = forms.CharField( widget=forms.TextInput(attrs={'title':'Current Date','style': 'width: 55% !important; resize: vertical !important;'}))
 
     shift_choices=(('APAC','APAC'),('EMEA','EMEA'),('USA','USA'))
 
-    Shift = forms.ChoiceField(choices=shift_choices, widget=forms.RadioSelect(attrs={'title':'The shift in which alert was handled'}))
+    Shift = forms.ChoiceField(choices=shift_choices, widget=forms.RadioSelect(attrs={'id':'shiftid','title':'The shift in which alert was handled','style': 'width: 75% !important; resize: vertical !important;'}))
 
     weekchoice=(('Weekday','Weekday'),('Weekend','Weekend'))
-    Day_Of_Week = forms.ChoiceField(choices=weekchoice, widget=forms.RadioSelect)
+    Day_Of_Week = forms.ChoiceField(choices=weekchoice, widget=forms.RadioSelect(attrs={'id':'weekid'}))
 
-    Start_Time = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'HH:MM','title':'Mention the start time in 24 hour time formant'}))
+    Start_Time = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'HH:MM','title':'Mention the start time in 24 hour time formant','style': 'width: 55% !important; resize: vertical !important;'}))
 
     #placeholder="HH:MM" size="13" title="Enter the time you spent" pattern=".+:.+"/
-    Responded_Time = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'HH:MM','title':'Mention the Responded time in 24 hour time formant'}))
+    Responded_Time = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'HH:MM','title':'Mention the Responded time in 24 hour time formant','style': 'width: 55% !important; resize: vertical !important;'}))
 
-    Time_spent = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'HH:MM','title':'Mention the Total time spent in 24 hour time formant'}))
-
+    Time_spent = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'HH:MM','title':'Mention the Total time spent in 24 hour time formant','style': 'width: 55% !important; resize: vertical !important;'}))
     MONTH_CHOICES = (
     ('January', 'January'),
     ('February', 'February'),
@@ -51,7 +50,7 @@ class CreateArticle(forms.ModelForm):
     #current_month = months[today.month]
     #months = today.month
     Month_empty = tuple(BLANK_CHOICE_DASH + list(MONTH_CHOICES))
-    Month = forms.ChoiceField(choices=(Month_empty), widget=forms.Select(attrs={'title':'Enter the current Month'}))
+    Month = forms.ChoiceField(choices=(Month_empty), widget=forms.Select(attrs={'title':'Enter the current Month','style': 'width: 55% !important; resize: vertical !important;'}))
 
     Res_team = (
     ('B2B - Core', 'B2B - Core'),
@@ -82,10 +81,10 @@ class CreateArticle(forms.ModelForm):
     ('Webapp', 'Webapp'),
 )
     Res_team_empty =  tuple(BLANK_CHOICE_DASH + list(Res_team))
-    Responsible_Team = forms.ChoiceField(choices=(Res_team_empty), widget=forms.Select(attrs={'title':'Enter the Responsible Team'}))
+    Responsible_Team = forms.ChoiceField(choices=(Res_team_empty), widget=forms.Select(attrs={'title':'Enter the Responsible Team','style': 'width: 55% !important; resize: vertical !important;'}))
 
     fse_alarm=(('Yes','Yes'),('No','No'))
-    False_Alarm = forms.ChoiceField(choices=fse_alarm, widget=forms.RadioSelect(attrs={'title':'Was the alert genuine?'}))
+    False_Alarm = forms.ChoiceField(choices=fse_alarm, widget=forms.RadioSelect(attrs={'id':'falseid','title':'Was the alert genuine?'}))
 
     inc_type = (
     ('Alert', 'Alert'),
@@ -94,10 +93,10 @@ class CreateArticle(forms.ModelForm):
     ('Task', 'Task'),
 )
     inc_type_empty = tuple(BLANK_CHOICE_DASH + list(inc_type))
-    Incident_Type = forms.ChoiceField(choices=(inc_type_empty), widget=forms.Select(attrs={'title':'Enter the type of incident'}))
+    Incident_Type = forms.ChoiceField(choices=(inc_type_empty), widget=forms.Select(attrs={'title':'Enter the type of incident','style': 'width: 55% !important; resize: vertical !important;'}))
 
     prior=(('P0','P0'),('P1','P1'),('P2','P2'),('None','None'))
-    Priority = forms.ChoiceField(choices=prior, widget=forms.RadioSelect(attrs={'title':'The priority of alert as per policy'}))
+    Priority = forms.ChoiceField(choices=prior, widget=forms.RadioSelect(attrs={'id':'priorityid','title':'The priority of alert as per policy'}))
 
     GHC = (
       ('Celery Dashboard check', 'Celery Dashboard check'),
@@ -107,7 +106,7 @@ class CreateArticle(forms.ModelForm):
       ('NA', 'NA'),
     )
     ghc_empty = tuple(BLANK_CHOICE_DASH + list(GHC))
-    General_Health_Check = forms.ChoiceField(choices=(ghc_empty), required=False, widget=forms.Select())
+    General_Health_Check = forms.ChoiceField(choices=(ghc_empty), required=False, widget=forms.Select(attrs={'title':'Name of the GHC','style': 'width: 55% !important; resize: vertical !important;'}))
 
     Alert = (
        ('AWS Notifications', 'AWS Notifications'),
@@ -128,7 +127,7 @@ class CreateArticle(forms.ModelForm):
        ('NA', 'NA'),
        )
     alert_empty = tuple(BLANK_CHOICE_DASH + list(Alert))
-    Alert = forms.ChoiceField(choices=(alert_empty), required=False, widget=forms.Select())
+    Alert = forms.ChoiceField(choices=(alert_empty), required=False, widget=forms.Select(attrs={'title':'Name of the Alert','style': 'width: 55% !important; resize: vertical !important;'}))
 
     Task = (
     ('Backup Audit', 'Backup Audit'),
@@ -144,10 +143,10 @@ class CreateArticle(forms.ModelForm):
     ('NA', 'NA'),
     )
     task_empty = tuple(BLANK_CHOICE_DASH + list(Task))
-    Task = forms.ChoiceField(choices=(task_empty), required=False, widget=forms.Select())
+    Task = forms.ChoiceField(choices=(task_empty), required=False, widget=forms.Select(attrs={'title':'Name of the task','style': 'width: 55% !important; resize: vertical !important;'}))
 
 
-    If_Others_Please_Specify = forms.CharField(initial=weeknumber, widget=forms.TextInput(attrs={'title':'If you entered the incident type as others, Please shed some light.'}))
+    If_Others_Please_Specify = forms.CharField(initial=weeknumber, widget=forms.TextInput(attrs={'title':'If you entered the incident type as others, Please shed some light.','style': 'width: 55% !important; resize: vertical !important;'}))
 
     envi = (
     ('Production', 'Production'),
@@ -159,7 +158,7 @@ class CreateArticle(forms.ModelForm):
     ('NA', 'NA'),
 )
     envi_empty = tuple(BLANK_CHOICE_DASH + list(envi))
-    Environment = forms.ChoiceField(choices=(envi_empty), widget=forms.Select(attrs={'title':'Enter the incident Environment'}))
+    Environment = forms.ChoiceField(choices=(envi_empty), widget=forms.Select(attrs={'title':'Enter the incident Environment','style': 'width: 55% !important; resize: vertical !important;'}))
 
     hosttype = (
     ('AWS Batch', 'AWS Batch'),
@@ -187,7 +186,7 @@ class CreateArticle(forms.ModelForm):
     ('Thrift Cluster', 'Thrift Cluster'),
 )
     hosttype_empty = tuple(BLANK_CHOICE_DASH + list(hosttype))
-    Host_Type = forms.ChoiceField(choices=(hosttype_empty), widget=forms.Select(attrs={'title':'Enter the host type'}))
+    Host_Type = forms.ChoiceField(choices=(hosttype_empty), widget=forms.Select(attrs={'title':'Enter the host type','style': 'width: 55% !important; resize: vertical !important;'}))
 
     source = (
     ('AWS', 'AWS'),
@@ -204,10 +203,10 @@ class CreateArticle(forms.ModelForm):
     ('Threatstack', 'Threatstack'),
 )
     source_empty = tuple(BLANK_CHOICE_DASH + list(source))
-    Source_of_Alert = forms.ChoiceField(choices=(source_empty), widget=forms.Select(attrs={'title':'Enter the source of alert'}))
+    Source_of_Alert = forms.ChoiceField(choices=(source_empty), widget=forms.Select(attrs={'title':'Enter the source of alert','style': 'width: 55% !important; resize: vertical !important;'}))
 
-    mode=[('Email','Email'),('Manual Check','Manual Check'),('Slack','Slack')]
-    Mode_of_Alert = forms.ChoiceField(choices=mode, widget=forms.RadioSelect(attrs={'title':'Enter the mode of alert'}), initial="Manual Check")
+    mode=[('Email','Email'),('Manual','Manual'),('Slack','Slack')]
+    Mode_of_Alert = forms.ChoiceField(choices=mode, widget=forms.RadioSelect(attrs={'id':'modeid','title':'Enter the mode of alert'}), initial="Manual")
 
     engineer= (
     ('Ajmeer Khaja', 'Ajmeer Khaja'),
@@ -225,12 +224,12 @@ class CreateArticle(forms.ModelForm):
     ('Vijay Mohan', 'Vijay Mohan'),
 )
     engineer_empty = tuple(BLANK_CHOICE_DASH + list(engineer))
-    NOC_Engineer = forms.ChoiceField(choices=(engineer_empty),widget=forms.Select(attrs={'title':'The NOC Engineer who handled the incident'}))
+    NOC_Engineer = forms.ChoiceField(choices=(engineer_empty),widget=forms.Select(attrs={'title':'The NOC Engineer who handled the incident','style': 'width: 55% !important; resize: vertical !important;'}))
 
     escalate=[('Yes','Yes'),('No','No')]
     #Escalated = forms.ChoiceField(choices=escalate, widget=forms.RadioSelect, initial='no')
     escalate_empty = tuple(BLANK_CHOICE_DASH + list(escalate))
-    Escalated = forms.ChoiceField(choices=(escalate_empty), initial='No', widget=forms.Select(attrs={'title':'Was the alert escalated?'}))
+    Escalated = forms.ChoiceField(choices=(escalate_empty), initial='No', widget=forms.Select(attrs={'title':'Was the alert escalated?','style': 'width: 55% !important; resize: vertical !important;'}))
 
     escreason = (
     ('Code level issue', 'Code level issue'),
@@ -242,7 +241,7 @@ class CreateArticle(forms.ModelForm):
     ('NA', 'NA'),
 )
     escreason_empty = tuple(BLANK_CHOICE_DASH + list(escreason))
-    Escalated_Reason = forms.ChoiceField(choices=(escreason_empty), required=False, widget=forms.Select(attrs={'title':'The reson for escalation'}))
+    Escalated_Reason = forms.ChoiceField(choices=(escreason_empty), required=False, widget=forms.Select(attrs={'title':'The reson for escalation','style': 'width: 55% !important; resize: vertical !important;'}))
 
     stat = (
     ('Resolved', 'Resolved'),
@@ -251,7 +250,7 @@ class CreateArticle(forms.ModelForm):
     ('Handed over', 'Handed over')
     )
     stat_empty = tuple(BLANK_CHOICE_DASH + list(stat))
-    Status = forms.ChoiceField(choices=(stat_empty),widget=forms.Select(attrs={'title':'Current status of the incident'}))
+    Status = forms.ChoiceField(choices=(stat_empty),widget=forms.Select(attrs={'title':'Current status of the incident','style': 'width: 55% !important; resize: vertical !important;'}))
 
     escto = (
     ('B2B core', 'B2B core'),
@@ -282,7 +281,7 @@ class CreateArticle(forms.ModelForm):
     ('Webappp', 'Webappp'),
 )
     escto_empty = tuple(BLANK_CHOICE_DASH + list(escto))
-    Escalated_to = forms.ChoiceField(choices=(escto_empty), required=False, widget=forms.Select(attrs={'title':'Enter the Team to whom the alert was escalated'}))
+    Escalated_to = forms.ChoiceField(choices=(escto_empty), required=False, widget=forms.Select(attrs={'title':'Enter the Team to whom the alert was escalated','style': 'width: 55% !important; resize: vertical !important;'}))
 
     resby = (
     ('B2B core', 'B2B core'),
@@ -313,13 +312,21 @@ class CreateArticle(forms.ModelForm):
     ('Webappp', 'Webappp'),
 )
     resby_empty = tuple(BLANK_CHOICE_DASH + list(resby))
-    Resolved_by_Team = forms.ChoiceField(choices=(resby_empty), widget=forms.Select(attrs={'title':'Enter the Team which resolved the alert'}))
+    Resolved_by_Team = forms.ChoiceField(choices=(resby_empty), widget=forms.Select(attrs={'title':'Enter the Team which resolved the alert','style': 'width: 55% !important; resize: vertical !important;'}))
 
-    Resolved_by_Engineer = forms.CharField(widget=forms.TextInput(attrs={'title':'The Engineer who resolved the alert'}))
+    Resolved_by_Engineer = forms.CharField(widget=forms.TextInput(attrs={'title':'The Engineer who resolved the alert','style': 'width: 55% !important; resize: vertical !important;'}))
 
+    Description = forms.CharField(widget=forms.Textarea(attrs={'title':'Description of the alert','style': 'width: 55% !important; resize: vertical !important;'}))
+
+    Remediation = forms.CharField(widget=forms.Textarea(attrs={'title':'Remediation done!','style': 'width: 55% !important; resize: vertical !important;'}))
+
+    Resolution = forms.CharField(widget=forms.Textarea(attrs={'title':'Resolution!','style': 'width: 55% !important; resize: vertical !important;'}))
+
+    Comments = forms.CharField(widget=forms.Textarea(attrs={'title':'Comments if any!','style': 'width: 55% !important; resize: vertical !important;'}))
+
+    Host_Name = forms.CharField(widget=forms.TextInput(attrs={'title':'Host name of the alert','style': 'width: 55% !important; resize: vertical !important;'}))
     class Meta:
         model = models.Post
-        fields = '__all__'
         field_order = ['Year','Week','Month','Date','Shift','Day_Of_Week','Time','Responded_Time','Time_spent','Complexity','Responsible_Team','False_alarm','Incident_Type','Priority','If_Others_Please_Specify','Description','Environment','Host_Type','Host_Name','Source_of_Alert','Mode_of_Alert','NOC_Engineer','Remediation','Escalated',
         'Escalated_Reason','Status','Escalated_to','Resolved_by_Team','Resolved_by_Engineer','Resolution','Comments']
         exclude = ['Ticket_Number','author']
