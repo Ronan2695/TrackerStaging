90a50ef8d5b755091a6e215a2ed0cc6cac01d343
rameshkrishna10 <ramesh.krishna@adroll.com>
2019-06-06 20:05:53 +0530

    changed debug to false, added amudha, increased timeout test

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&7f1+wlw6x%mmscfn&6#4%))it&uc4r=y$d!e5qo2a4eti_9n5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
